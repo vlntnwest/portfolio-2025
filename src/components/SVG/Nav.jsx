0099fc39bdaf502d2008011d764a397176aa8df8
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2025-11-12 19:41:33 +0100

    Wheel ui

---
+++ src/components/SVG/Nav.jsx
@@ -0,0 +1,20 @@
+const Nav = ({ name, className }) => {
+  return (
+    <svg
+      id={name}
+      data-name={name}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 62.56 36.45"
+      className={className}
+    >
+      <g id={name} data-name={name}>
+        <polygon
+          class="cls-1"
+          points="51.79 0 51.79 16.33 25.9 0 25.9 16.33 0 0 0 36.45 25.9 20.12 25.9 36.45 51.79 20.12 51.79 36.45 62.56 36.45 62.56 0 51.79 0"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default Nav;
---
+++ src/components/SVG/Play.jsx
@@ -0,0 +1,21 @@
+const Play = ({ className }) => {
+  return (
+    <svg
+      id="Play"
+      data-name="Play"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 78.7 36.45"
+      className={className}
+    >
+      <g id="Play" data-name="Play">
+        <g>
+          <polygon className="cls-1" points="0 36.45 0 0 28.9 18.23 0 36.45" />
+          <rect className="cls-1" x="46.97" width="10.76" height="36.45" />
+          <rect className="cls-1" x="67.93" width="10.76" height="36.45" />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default Play;
