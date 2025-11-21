8fd77979ff687d9785ea56fbe2356e66896b4a90
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2025-11-21 20:03:59 +0100

    Init carousel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,7 @@
-"use client";
-import { useWheelContext } from "../../contexts/WheelContext";
 import projects from "@/lib/projects.json";
 import MenuLink from "../ui/buttons/MenuLink";
 
 const Header = () => {
-  const { mode } = useWheelContext();
   return (
     <header>
       <div className="bg-black">{projectsList}</div>
