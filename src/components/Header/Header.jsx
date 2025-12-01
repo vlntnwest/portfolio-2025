e2fa4c80089d4efd6d30263069bed4afb54169aa
Valentin WESTERMEYER <valentin@westermeyer.fr>
2025-12-01 06:50:35 +0100

    header social links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import ContactContainer from "./ContactContainer";
 import ContactBubbles from "./ContactBubbles";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="absolute top-0 left-0 right-0 p-6">
       <div className="flex justify-center items-center">
         <div className="flex flex-col items-center ">
-          <span className="rounded-full overflow-hidden bg-linear-to-b from-[#FF3C3C] to-[#5F1616]">
+          <Link
+            href="/"
+            className="rounded-full overflow-hidden bg-linear-to-b from-[#FF3C3C] to-[#5F1616] z-10"
+          >
             <Image
               src="/assets/images/memoji/Classic.PNG"
               width={80}
@@ -15,7 +19,7 @@ const Header = () => {
               alt="Memoji Valentin WESTERMEYER"
               className="h-20 w-20"
             />
-          </span>
+          </Link>
           <ContactContainer>
             <ContactBubbles />
           </ContactContainer>
