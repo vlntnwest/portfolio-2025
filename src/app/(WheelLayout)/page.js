784147bf9285e1dc5117db830465326890ffa474
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2025-12-15 18:32:03 +0100

    overflow-hiden homepage

--- src/app/(WheelLayout)/page.js
+++ src/app/(WheelLayout)/page.js
@@ -1,6 +1,14 @@
+"use client";
 import ProjectsWrapper from "@/components/Project/ProjectsWrapper";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
   return (
     <div className="overflow-hidden flex flex-col h-[calc(100vh-var(--header-height))]">
       <div className="relative h-auto">
