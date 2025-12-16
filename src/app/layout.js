dc79c26295c75abca227b6587af0bba9e48f4725
Valentin WESTERMEYER <valentin@westermeyer.fr>
2025-12-16 12:07:21 +0100

    seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,30 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Valentin Westermeyer",
-  description: "Portfolio 2025 Valentin Westermeyer, développeur à Strasbourg",
+  title: {
+    default:
+      "Valentin Westermeyer - Développeur Next.js et React basé à Strasbourg",
+    template: "%s | Valentin Westermeyer",
+  },
+  description:
+    "Portfolio 2025 Valentin Westermeyer, développeur web à Strasbourg",
+  openGraph: {
+    title: "Valentin Westermeyer | Développeur Next.js à Strasbourg",
+    description:
+      "Portfolio 2025 de Valentin Westermeyer, développeur web à Strasbourg spécialisé en Next.js et React. Création de sites web modernes et performants.",
+    url: "https://valentin-westermeyer.fr",
+    siteName: "Valentin Westermeyer - Développeur Web",
+    images: [
+      {
+        url: "https://valentin-westermeyer.fr/og-image.jpg", // Créez une image spécifique pour le partage (1200x630px idéalement)
+        width: 1200,
+        height: 630,
+        alt: "Valentin Westermeyer - Portfolio web",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
